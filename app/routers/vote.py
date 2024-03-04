e00addb049530d9dbe332c23aace2801d8fa730c
ritesh <riteshsingh9827@gmail.com>
2024-03-04 21:47:38 +0530

    fixed vote router; method

--- app/routers/vote.py
+++ app/routers/vote.py
@@ -8,7 +8,7 @@ router = APIRouter(
 )
 
 
-@router.get("/", status_code=status.HTTP_201_CREATED)
+@router.post("/", status_code=status.HTTP_201_CREATED)
 def vote(vote: schemas.Vote, db: Session = Depends(database.get_db), current_user: int = 
          Depends(oauth2.get_current_user)):
     
